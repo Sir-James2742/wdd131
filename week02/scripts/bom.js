40ad833ba29e7dce62d099102c0ef36ccd122942
Sir-James2742 <omondijames2742@gmail.com>
2025-07-09 18:49:17 +0300

    redoing of assignment

---
+++ week02/scripts/bom.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+const input = document.querySelector('#favchap');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.appendChild(deleteButton);
+list.appendChild(li);
