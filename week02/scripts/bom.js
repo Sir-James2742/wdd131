358536fc646ad8425a3a5aa1a277c5c75f227c06
Sir-James2742 <omondijames2742@gmail.com>
2025-07-11 12:02:28 +0300

    adding event listeners

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,28 @@ const button = document.querySelector('button');
 const input = document.querySelector('#favchap');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.appendChild(deleteButton);
-list.appendChild(li);
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+        input.focus();
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+    }
+    input.value = '';
+    input.focus();
+
+
+});
+
